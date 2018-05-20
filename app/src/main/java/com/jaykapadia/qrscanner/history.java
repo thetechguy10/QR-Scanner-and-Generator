4568a248dc2043cc3bce4e0503574c1006e1325d
thetechguy10 <31533247+thetechguy10@users.noreply.github.com>
2018-05-20 10:17:52 +0530

    Update history.java

--- app/src/main/java/com/jaykapadia/qrscanner/history.java
+++ app/src/main/java/com/jaykapadia/qrscanner/history.java
@@ -38,27 +38,32 @@ public class history extends AppCompatActivity {
          SharedPreferences sp = getSharedPreferences("list", Context.MODE_PRIVATE);
         Map<String, ?> allEntries = sp.getAll();
       int size = allEntries.entrySet().size();
-      int siez = size-1;
-        final String[] display = new String [siez];
-      for(int i=0;i<siez;i++){
-          String temp =  sp.getString(String.valueOf(i),"");
-          final Barcode b1 = gson.fromJson(temp,Barcode.class);
-          String pr = b1.displayValue;
-          display[i]= pr;
+      if(size == 0){
+          final String[] display = getResources().getStringArray(R.array.history);
+          ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,display);
+          l1.setAdapter(adapter);
+      }else{
+          int siez = size-1;
+          final String[] display = new String [siez];
+          for(int i=0;i<siez;i++){
+              String temp =  sp.getString(String.valueOf(i),"");
+              final Barcode b1 = gson.fromJson(temp,Barcode.class);
+              String pr = b1.displayValue;
+              display[i]= pr;
+          }
+          Collections.reverse(Arrays.asList(display));
+          ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,display);
+          l1.setAdapter(adapter);
+          l1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+              @Override
+              public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                  int siez = display.length;
+                  int x = siez-i-1;
+                  action(x);
+              }
+          });
       }
 
-
-        Collections.reverse(Arrays.asList(display));
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,display);
-        l1.setAdapter(adapter);
-        l1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                int siez = display.length;
-                int x = siez-i-1;
-                action(x);
-            }
-        });
     }
 
 public void action(int i){
